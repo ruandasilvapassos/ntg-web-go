f85e3f972246e7b83a1dd6cd3d52895606c5630b
aldycavalera <personal.cavalera@gmail.com>
2022-05-05 16:56:14 +0700

    feat(sentry): add sentry to strapi using plugin

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -11,5 +11,12 @@ module.exports = ({ env }) => ({
         defaultPath: 'assets'
       }
     }
+  },
+  sentry: {
+    enabled: true,
+    config: {
+      dsn: env('SENTRY_DSN'),
+      sendMetadata: true
+    }
   }
 })
